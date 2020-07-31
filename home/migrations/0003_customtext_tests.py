10a90b0e1f0b9577ef88599aac293095e37cf9b4
Shah Rukh Khan <shah@crowdbotics.com>
2020-07-31 13:38:00 +0000

    Update models

--- home/migrations/0003_customtext_tests.py
+++ home/migrations/0003_customtext_tests.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0002_load_initial_data'),
+        ("home", "0002_load_initial_data"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='customtext',
-            name='tests',
+            model_name="customtext",
+            name="tests",
             field=models.BigIntegerField(blank=True, null=True),
         ),
     ]
--- home/models.py
+++ home/models.py
@@ -8,7 +8,6 @@ from django.db import models
 
 class CustomText(models.Model):
     title = models.CharField(max_length=150,)
-    tests = models.BigIntegerField(null=True, blank=True,)
 
     def __str__(self):
         return self.title
@@ -24,6 +23,7 @@ class CustomText(models.Model):
 
 class HomePage(models.Model):
     body = models.TextField()
+    line = models.BigIntegerField(null=True, blank=True,)
 
     @property
     def api(self):
